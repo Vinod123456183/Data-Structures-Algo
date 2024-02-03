b4aef72fe9bdc5ef15bd7e9f2c4afaa86411225e
Vinod SIngh <115033744+Vinod123456183@users.noreply.github.com>
2024-02-03 12:35:06 +0530

    Create Basic11.cpp

---
+++ Recursion/Basic11.cpp
@@ -0,0 +1,23 @@
+/*
+  Reveerse Array Using REc
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rev(vector<int>&v , int low , int high)  {
+
+    // int l  = 0 , high = v.size()-1;
+    if(low>high)return ; 
+    swap(v[low++] , v[high--]);
+    rev(v , low+1 , high-1);
+
+}
+
+int main(){
+  vector<int>v{1,2,3,4,6,7,8};
+  int low = 0;
+  rev(v , low , v.size()-1);
+  for(int i =0 ; i <v.size(); i++ ) cout<<v[i]<<" ";
+  // for(int i =0 ; i <v.size(); i++ ) cout<<v[i]<<" ";    
+}
